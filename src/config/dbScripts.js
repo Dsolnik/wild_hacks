b54f17570f64b6a7dd755f4e30678ec4720142c5
codercahol <codercahol@gmail.com>
2017-11-04 16:26:48 -0400

    fixed dbScrips

--- src/config/dbScripts.js
+++ src/config/dbScripts.js
@@ -1,15 +1,19 @@
 /*jshint node: true */
 
-function monthLen (abbrev) {
-    var thirty = ["apr","jun","sept","nov"];
-    if (abbrev=="feb") return 28;
+//monthID as int from Date().getMonth()
+function monthLen (monthID) {
+    //months with 30 days: april, june, september november
+    var thirty = [3,5,8,10];
+    //month is feb
+    if (monthID==1) return 28;
     else if (thirty.indexOf(abbrev)>=0) return 30;
     else return 31
 };
 
+//generate calendar from start date
 function genCal(start, thisMonth) {
     var monthCal = new Array
-    for (var i = 0; i < monthLen(thisMonth); i++ ){
+    for (var i = 1; i <= monthLen(thisMonth); i++ ){
         if (i < start) monthCal[i] = -1;
         else monthCal[i] = 0;
     }
