3ce050c19a9dc4687b8d15616fecb27581cbc105
Helen Chen <helenjoychen@gmail.com>
2017-11-04 16:19:06 -0500

    working javascript for groups

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,30 @@
 window.onload = function(){
+  if(document.title == "Groups"){
+    var newGroup = document.getElementById('newGroup');
+    var create_newGroup = document.getElementById('create_newGroup');
 
-  var signIn = document.getElementById('signIn');
-  var signUp = document.getElementById('signUp');
-  var toggle_signUp = document.getElementById('toggle_signUp');
-  var toggle_signIn = document.getElementById('toggle_signIn');
+    create_newGroup.addEventListener('click', function(){
+      if(newGroup.style.display == "block"){
+        newGroup.style.display = "none";
+      }else{
+        newGroup.style.display = "block";
+      }
+    });
+  }else{
+    var signIn = document.getElementById('signIn');
+    var signUp = document.getElementById('signUp');
+    var toggle_signUp = document.getElementById('toggle_signUp');
+    var toggle_signIn = document.getElementById('toggle_signIn');
 
-  toggle_signIn.addEventListener('click', function(){
-    signUp.style.display = "none";
-    signIn.style.display = "block";
-  });
+    toggle_signIn.addEventListener('click', function(){
+      signUp.style.display = "none";
+      signIn.style.display = "block";
+    });
 
-  toggle_signUp.addEventListener('click', function(){
-    signUp.style.display = "block";
-    signIn.style.display = "none";
-  });
-
-  var newGroup = document.getElementById('newGroup');
-  var create_newGroup = document.getElementById('create_newGroup');
-
-  create_newGroup.addEventListener('click', function(){
-    newGroup.style.display = "block";
-  });
+    toggle_signUp.addEventListener('click', function(){
+      signUp.style.display = "block";
+      signIn.style.display = "none";
+    });
+  }
 
 };
