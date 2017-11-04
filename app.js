8ceccba7ad5c28573f7540e26450e4876d74cf90
Daniel <dansolnik@gmail.com>
2017-11-04 00:17:59 -0500

    set up routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /*jshint node: true */
 var express = require('express');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser')
 
 var app = express();
 
--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -1,11 +1,17 @@
+/*jshint node: true */
+
 var express = require('express');
 var authRouter = express.Router();
 
 var router = function() {
-    authRouter.route('/signUp').post(function(req, res){
-        console.log(req.body); 
-    });
+    
+    authRouter.route('/signUp')
+        .post(function(req, res){
+            console.log(req.body); 
+            // put in checking the user here
+        });
+    
     return authRouter;
-}
+};
 
 module.exports = router;
