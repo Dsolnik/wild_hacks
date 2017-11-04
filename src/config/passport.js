35a2bd896d87c1df86844996055cfbf9c2c42c1b
codercahol <codercahol@gmail.com>
2017-11-04 03:00:42 -0400

    commented out mongoose functionality

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 
 var passportFunction = function(app) {
 
-    app.use(passport.initalize());
+    app.use(passport.initialize());
     app.use(passport.session());
       
 };
