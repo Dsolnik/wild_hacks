d14ce0bd9248b8e57e586df2e4d618b3c5645e81
Daniel <dansolnik@gmail.com>
2017-11-04 10:58:00 -0500

    merged

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -4,28 +4,17 @@ var express = require('express');
 var authRouter = express.Router();
 var passport = require('passport');
 var models = require('../models/models');
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 451d92423a73311a92368bf9986fb5f5dd00c885
 var router = function () {
 
     authRouter.route('/signUp')
         .post(function (req, res) {
             console.log('signup here', req.body);
             // insert the user into the database HERE
-<<<<<<< HEAD
-            var user = new models.User();
-            user.name = req.body.name;
-            user.email = req.body.email;
-=======
             var user = new models.User({
                 name : req.body.name,
                 email : req.body.email
             });
             user.save();
->>>>>>> 451d92423a73311a92368bf9986fb5f5dd00c885
             // replace req.body with the object of the user
             req.login(user, function () {
                 res.redirect('/auth/dashboard');
