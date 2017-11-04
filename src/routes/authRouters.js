6e28c95ce08c9548dd8de7cd76d50a7db4d9a2ae
Daniel <dansolnik@gmail.com>
2017-11-04 09:50:09 -0500

    good morning #2

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -5,15 +5,16 @@ var authRouter = express.Router();
 var passport = require('passport');
 
 
-var router = function (wagner) {
+var router = function () {
 
     authRouter.route('/signUp')
         .post(function (req, res) {
             console.log('signup here', req.body);
             // insert the user into the database HERE
-            var user = new wagner.User();
+//            var user = new User();
             user.name = req.body.name;
             user.email = req.body.email;
+            user.save();
             // replace req.body with the object of the user
             req.login(req.body, function () {
                 res.redirect('/auth/dashboard');
@@ -26,10 +27,18 @@ var router = function (wagner) {
         .post(passport.authenticate('local', {
             failureRedirect: '/'
         }), function (req, res) {
-            console.log("WE GOOD!");
+            console.log("Sucessful Sign in!");
             res.redirect('/auth/dashboard');
         });
 
+    authRouter.route('/dashboard')
+        .get(function (req, res, next){
+            if(!req.user){
+                res.redirect('/');
+            }
+            next();
+    });
+    
     authRouter.route('/dashboard')
         .get(function (req, res) {
             res.json(req.user);
