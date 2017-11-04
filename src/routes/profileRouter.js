53fae5c2befeaf8fe1462b6e6cc64621e66f37c8
codercahol <codercahol@gmail.com>
2017-11-04 16:06:47 -0400

    abstracted new fns

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -4,27 +4,11 @@ var models = require('../models/models');
 var status = require('http-status');
 
 var profileRouter = express.Router();
-//var groupRouter = require('./groupRouter');
-
-function monthLen (abbrev) {
-    var thirty = ["apr","jun","sept","nov"];
-    if (abbrev=="feb") return 28;
-    else if (thirty.indexOf(abbrev)>=0) return 30;
-    else return 31
-};
-
-function genCal(start, thisMonth) {
-    var monthCal = new Array
-    for (var i = 0; i < monthLen(thisMonth); i++ ){
-        if (i < start) monthCal[i] = -1;
-        else monthCal[i] = 0;
-    }
-    return monthCal;
-};
+var groupRouter = require('./groupRouter');
 
 module.exports = function () {
     
-    //profileRouter.use('/group', groupRouter);
+    profileRouter.use('/group', groupRouter);
 
     profileRouter.get('/dashboard', function(req, res){
         res.render('dashboard');
