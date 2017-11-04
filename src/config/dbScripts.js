44018d1f326a559a8fbc63c2cdcfa1c5f6e29344
codercahol <codercahol@gmail.com>
2017-11-04 17:56:00 -0400

    record appends new month at month change now

--- src/config/dbScripts.js
+++ src/config/dbScripts.js
@@ -6,17 +6,19 @@ function monthLen (monthID) {
     var thirty = [3,5,8,10];
     //month is feb
     if (monthID==1) return 28;
-    else if (thirty.indexOf(abbrev)>=0) return 30;
+    else if (thirty.indexOf(monthID)>=0) return 30;
     else return 31
 };
 
-//generate calendar from start date
+//generate calendar from start date 
 function genCal(start, thisMonth) {
     var monthCal = new Array
     for (var i = 1; i <= monthLen(thisMonth); i++ ){
-        if (i < start) monthCal[i] = -1;
-        else monthCal[i] = 0;
+        if (i < start) monthCal[i-1] = -1;
+        // i-1 bc i is 0-indexed and start is 1-31
+        else monthCal[i-1] = 0;
     }
+    monthCal.month = thisMonth;
     return monthCal;
 };
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,17 @@
 var mongoose = require('mongoose');
 var scripts  = require('../config/dbScripts');
 
-var userSchema = {
+module.exports = new mongoose.Schema({
     info: {
         name: { type: String, required: true },
         email: { type: String },
-        password: { type: String},
-        start_date: { type: String},
-        record: [[{ type: Number }]]
+        password: { type: String },
         // more later? ie payment info
     },
+    start_date: { type: Date},
+    current_date: { type: Date},
+    record: [{ type: Number }],
     groups: [{
         id: { type: Number }
     }]
-
-};
-
-module.exports = new mongoose.Schema(userSchema);
+});
