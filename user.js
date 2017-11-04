037148b5b198292b7f01a51f742e8d843dbbba39
codercahol <codercahol@gmail.com>
2017-11-04 02:04:39 -0400

    group and user schema finished for now

--- user.js
+++ user.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 
 var userSchema = {
     _id: { type: String },
+    info: {
+        name: { type: String, required: true },
+        email: { type: String },
+        oauth: { type: String, required: true }
+        // more later? ie payment info
+    },
+    groups: [{
+        id: { type: Number }
+    }]
 
-}
+};
 
 module.exports = new mongoose.Schema(userSchema);
