6455acd6b9168b25006d5b060bbdc5f79a51296f
Daniel <dansolnik@gmail.com>
2017-11-04 13:27:00 -0500

    stuff works

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -8,6 +8,12 @@ var groupRouter = require('groupRouter');
 
 profileRouter.use('/group', groupRouter);
 
+profileRouter.use(function(req, res, next{
+                            if(!req.user) res.redirect('/login');
+                            
+                            
+                            }));
+
 profileRouter.get('/dashboard', function(req, res){
     // change to 'dashboard'
     return res.render('dashboard');
